373ed16e5377ddd5b5d1d8aadf0a162ac69edd6e
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2019-04-23 17:39:54 -0600

    refactored map component to include state so that viewport dynamically changes the state when adjusted by zooming or panning

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,34 +1,47 @@
 
-import React, {useState} from 'react'
-import ReactMapGL, {Marker} from 'react-map-gl';
+import React, {Component} from 'react'
+import ReactMapGL from 'react-map-gl';
 
-export default function Map() {
 
-    const TOKEN = "pk.eyJ1IjoibWFmb250YW5hIiwiYSI6ImNqcXptcnRucDA0bngzeW94Y3lvNm9hOWQifQ.Da7mHzfQYcdq9eXABgNPQQ"
-    const [viewport, setViewport] = useState({
-        width: "100vw",
-        height: "100vh",
-        latitude: 39.742043,
-        longitude: -104.991531,
-        zoom: 10
-      })
+const TOKEN = "pk.eyJ1IjoibWFmb250YW5hIiwiYSI6ImNqcXptcnRucDA0bngzeW94Y3lvNm9hOWQifQ.Da7mHzfQYcdq9eXABgNPQQ"
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        viewport: {
+            latitude: 39.742043,
+            longitude: -104.991531,
+            zoom: 10,
+            bearing: 0,
+            pitch: 0
+          }
+      
+    };
+  }
+
+  _updateViewport = (viewport) => {
+    this.setState({viewport});
+  }
+
+  render() {
+
+    const {viewport} = this.state
 
     return (
-      <div>
         <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={TOKEN}
+        width="100vw"
+        height="100vh"
         mapStyle="mapbox://styles/mafontana/cjuubnuzvhdnz1fmqsgojz8s3"
-        onViewportChange={viewport => {
-            setViewport(viewport)
-        }}
-        >
-        </ReactMapGL>
-    
-      </div>
-    )
+        onViewportChange={this._updateViewport}
+      >
+      </ReactMapGL>
+    );
   }
+}
 
-
+export default Map
 
 
