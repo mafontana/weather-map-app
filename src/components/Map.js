b6debd942be1f68c6e942e5ab28966fba9b5f428
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2019-04-23 16:01:11 -0600

    added a style to the map to change the look

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 
-import React, { useState} from 'react'
-import ReactMapGL from 'react-map-gl';
+import React, {useState} from 'react'
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 export default function Map() {
 
@@ -18,80 +18,17 @@ export default function Map() {
         <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/mafontana/cjuubnuzvhdnz1fmqsgojz8s3"
         onViewportChange={viewport => {
             setViewport(viewport)
         }}
         >
- 
         </ReactMapGL>
-      
-      
+    
       </div>
     )
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Map extends Component {
-    
-//     componentDidMount() {
-//         var map = new mapboxgl.Map({
-//             ...this.props,
-//             container: this.container
-//         })
-//     }
-    
-
-//     render(){
-//       return (
-//         <div ref={(x) => {this.container = x}}>
-        
-        
-        
-        
-//         </div>
-//       );
-//     }
-//   }
-
-
-
-
-
-// const Map = () => {
-//     var map = new mapboxgl.Map({
-//         style: 'mapbox://styles/mapbox/light-v9',
-//         center: [-74.0066, 40.7135],
-//         zoom: 15,
-//         container: 'map'
-
-//    })
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// export default Map;
 
 
 
