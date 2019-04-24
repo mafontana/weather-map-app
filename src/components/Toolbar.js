a507c0928d858c84dd480df0a941ef3a97adc27a
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2019-04-24 11:38:03 -0600

    created a toolbar component to allow user to see more detailed weather data

---
+++ src/components/Toolbar.js
@@ -0,0 +1,21 @@
+
+import React, {Component} from 'react'
+
+
+class Toolbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+        <div className="Toolbar">
+        
+      </div>
+    );
+  }
+}
+
+export default Toolbar
