af155f6e6dc9e3a57100366fc0d1eab6f83ccd92
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2019-04-25 10:40:01 -0600

    added favicon and changed app name

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -14,7 +14,7 @@ class Toolbar extends Component {
         <div className="toolbar">
         <header>
             <div className="toolbarHeader">
-                <img src={require("./weather_app_logo.png")} className="logo"></img>
+                <img src={require("../assets/weather_app_logo.png")} className="logo"></img>
                 <div className="toolbarText">
                     <h1>World Weather App</h1>
                     <p>Click on the map to get started</p>
